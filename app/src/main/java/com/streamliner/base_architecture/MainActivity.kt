3ecea0e9a34960e9bdc98baa37249f38832dcd7d
thisisstreamliner <thisisstreamliner@gmail.com>
2019-09-15 20:56:53 +0300

    Added start project files

---
+++ app/src/main/java/com/streamliner/base_architecture/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.streamliner.base_architecture
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
